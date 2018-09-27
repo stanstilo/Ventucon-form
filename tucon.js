dabbc2a563cce002422eaa82d152a59c1f0d81c4
stanstilo <36269273+stanstilo@users.noreply.github.com>
2018-09-27 13:45:12 +0100

    updated js

--- tucon.js
+++ tucon.js
@@ -1,9 +1,19 @@
-function onMenuClick(x){
- x.classList.toggle("change")
- $('#overlay').slideToggle(500);
- if(document.getElementById("overlay").style.height == 0){}
-}
+$( document ).ready(function() {
 
-function toggle(){
-    $('#overlay').css("height", "100%");
-}
+$( ".cross" ).hide();
+$( ".menu" ).hide();
+$( ".hamburger" ).click(function() {
+$( ".menu" ).slideToggle( "slow", function() {
+$( ".hamburger" ).hide();
+$( ".cross" ).show();
+});
+});
+
+$( ".cross" ).click(function() {
+$( ".menu" ).slideToggle( "slow", function() {
+$( ".cross" ).hide();
+$( ".hamburger" ).show();
+});
+});
+
+});
